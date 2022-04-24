5a5182361b9456fc88a4a917950ada55a83fc955
tishlakov.n <tishlakov.nn@gmail.com>
2022-04-24 18:49:06 +0300

    task-0003 add migrations

--- src/backend/database/migrations/2022_04_24_151209_create_categories_table.php
+++ src/backend/database/migrations/2022_04_24_151209_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,18 +12,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_unicode_ci';
             $table->id();
-            $table->string('name', 64);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_active')->default(true);
-            $table->foreignId('role_id')->constrained();
-            $table->rememberToken();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name', 64)->unique();
+            $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
     }
@@ -36,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
--- src/backend/database/migrations/2022_04_24_152919_create_products_table.php
+++ src/backend/database/migrations/2022_04_24_152919_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,18 +12,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_unicode_ci';
             $table->id();
-            $table->string('name', 64);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_active')->default(true);
-            $table->foreignId('role_id')->constrained();
-            $table->rememberToken();
+            $table->string('name', 128);
+            $table->string('article', 128);
+            $table->decimal('price')->default(0);
+            $table->foreignId('category_id')->constrained();
+            $table->text('description')->nullable();
+            $table->string('imgUrl')->nullable();
+            $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
     }
@@ -36,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
